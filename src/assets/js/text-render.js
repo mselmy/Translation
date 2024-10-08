89ab5b20750ee32c20047a20719e4366b62fda3d
Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-10-08 15:28:04 +0300

    Refactor text rendering function and add edit functionality

--- src/assets/js/text-render.js
+++ src/assets/js/text-render.js
@@ -1,14 +1,17 @@
+let unrenderEnglishText ="";
+let unrenderArabicText ="";
+
 function textRender() {
-    let englishText = document.getElementById('englishText').value;
-    let arabicText = document.getElementById('arabicText').value;
+    let englishText = unrenderEnglishText;
+    let arabicText = unrenderArabicText;
 
     // replace all "\n" with "<br>" even if another word is attached to it
     englishText = englishText.replace(/\\n/g, '<br>');
     arabicText = arabicText.replace(/\\n/g, '<br>');
 
-    // replace <div fontcolor=hexa_color> tages with <span style="color: hexa_color;"> tags with the same hexa color from div tages
-    englishText = englishText.replace(/<div fontcolor=([#a-fA-F0-9]+)>/g, '<span style="color: $1;">');
-    arabicText = arabicText.replace(/<div fontcolor=([#a-fA-F0-9]+)>/g, '<span style="color: $1;">');
+    // replace <div fontcolor=hexa_color> tages with <span style="color: hexa_color;"> tags with the same hexa color from div tages and add "#" if not exist
+    englishText = englishText.replace(/<div fontcolor=([0-9A-Fa-f]{6})>/g, '<span style="color: #$1;">');
+    arabicText = arabicText.replace(/<div fontcolor=([0-9A-Fa-f]{6})>/g, '<span style="color: #$1;">');
 
     // replace </div> tages with </span> tages
     englishText = englishText.replace(/<\/div>/g, '</span>');
@@ -18,10 +21,34 @@ function textRender() {
     englishText = englishText.replace(/%s/g, 'placeholder');
     arabicText = arabicText.replace(/%s/g, 'مثال');
 
-    console.log(englishText);
-    console.log(arabicText);
-
     // render the text by replacing the text area with div and render the text inside the div
     document.getElementById('englishText').outerHTML = '<div class="form-control" id="englishText" style="white-space: pre-line;">' + englishText + '</div>';
-    document.getElementById('arabicText').outerHTML = '<div class="form-control" id="arabicText" style="white-space: pre-line; direction: rtl;">' + arabicText + '</div>';
+    document.getElementById("arabicText").outerHTML =
+      '<div class="form-control" id="arabicText" style="white-space: pre-line; direction: rtl;">' +
+      arabicText +
+      "</div>";
 }
+
+// event listner for div with id "englishText" or "arabicText" to replace the div with text area again using unrender text
+document.addEventListener("click", function (e) {
+  if (e.target.id === "englishText" && e.target.tagName === "DIV") {
+    document.getElementById("englishText").outerHTML =
+      '<textarea class="form-control" id="englishText" rows="10">' +
+      unrenderEnglishText +
+      "</textarea>";
+  } else if (e.target.id === "arabicText" && e.target.tagName === "DIV") {
+    document.getElementById("arabicText").outerHTML =
+      '<textarea class="form-control" id="arabicText" rows="10">' +
+      unrenderArabicText +
+      "</textarea>";
+  }
+});
+
+// event listner for text area with id "englishText" or "arabicText" to save the text value in unrender text and render the text
+document.addEventListener("input", function (e) {
+  if (e.target.id === "englishText" && e.target.tagName === "TEXTAREA") {
+    unrenderEnglishText = e.target.value;
+  } else if (e.target.id === "arabicText" && e.target.tagName === "TEXTAREA") {
+    unrenderArabicText = e.target.value;
+  }
+});
